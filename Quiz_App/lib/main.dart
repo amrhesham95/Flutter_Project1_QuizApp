96b325cc0f70933e09ff14711a040798d8974b4d
Amr Hesham <amrhesham9595@gmail.com>
2021-02-26 13:24:45 +0200

    fix typo

--- Quiz_App/lib/main.dart
+++ Quiz_App/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApap extends StatefulWidget {
+class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }

dffdd882b98ee81a239dcb95cb603e113ae71c6d
Amr Hesham <amrhesham9595@gmail.com>
2021-02-27 14:59:42 +0200

    mapping rounds into questions and mapping questions into answers widgets

--- Quiz_App/lib/answer.dart
+++ Quiz_App/lib/answer.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
-
-  Answer(this.selectHandler);
+  final String answerText;
+  Answer(this.answerText, this.selectHandler);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
+        textColor: Colors.white,
         color: Colors.blue,
-        child: Text("Answer1"),
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
--- Quiz_App/lib/main.dart
+++ Quiz_App/lib/main.dart
@@ -14,9 +14,18 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questions = [
-    "What's your name?",
-    "Where are you from?",
-    "How old are you?"
+    {
+      "questionText": "What are you going to do tonight?",
+      "answers": ["Play ludo with Hagar", "Study", "Go to gym"],
+    },
+    {
+      "questionText": "What's your favorite food?",
+      "answers": ["Pasta", "Pizza", "Burger"],
+    },
+    {
+      "questionText": "Where are you from?",
+      "answers": ["Cairo", "Alex", "Luxor"],
+    },
   ];
 
   var _questionIndex = 0;
@@ -38,11 +47,12 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questionText: _questions[_questionIndex],
+              questionText: _questions[_questionIndex]["questionText"],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            ...(_questions[_questionIndex]["answers"] as List<String>)
+                .map((answer) {
+              return Answer(answer, _answerQuestion);
+            }).toList()
           ],
         ),
       ),

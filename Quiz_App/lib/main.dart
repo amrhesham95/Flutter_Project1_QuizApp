79bd43150e7757dc943273eecc9b23e7e1d7ab80
Amr Hesham <amrhesham9595@gmail.com>
2021-02-28 22:55:25 +0200

    implementing reset action

--- Quiz_App/lib/main.dart
+++ Quiz_App/lib/main.dart
@@ -44,12 +44,19 @@ class _MyAppState extends State<MyApp> {
   var _totalScore = 0;
 
   _answerQuestion(int score) {
-    // _totalScore += score;
+    _totalScore += score;
     setState(() {
       _questionIndex++;
     });
 
     print(_questionIndex);
+  }
+
+  _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
   }
 
   Widget build(BuildContext context) {
@@ -64,9 +71,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 answerQuestion: _answerQuestion,
               )
-            : Result(
-                totalScore: _totalScore,
-              ),
+            : Result(totalScore: _totalScore, resetHandler: _resetQuiz),
       ),
     );
   }
--- Quiz_App/lib/result.dart
+++ Quiz_App/lib/result.dart
@@ -3,13 +3,22 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int totalScore;
 
-  Result({@required this.totalScore});
+  final Function resetHandler;
+  Result({@required this.totalScore, @required this.resetHandler});
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        totalScore.toString(),
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      child: Column(
+        children: [
+          Text(
+            totalScore.toString(),
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          RaisedButton(
+            onPressed: resetHandler,
+            child: Text("Reset"),
+          ),
+        ],
       ),
     );
   }
